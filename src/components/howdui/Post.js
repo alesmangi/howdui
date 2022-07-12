17c9c6c404112d33035c5d24e00d94c5e51dd3fb
alansam460 <55721462+alansam460@users.noreply.github.com>
2022-07-12 18:23:44 -0400

    Added like button

--- src/components/howdui/Post.js
+++ src/components/howdui/Post.js
@@ -1,15 +1,30 @@
 import React from "react"
-import { Navbar, Nav, Card, Container } from "react-bootstrap"
+import { Navbar, Nav, Card, Container, Button } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import Submit from "./Submit"
 import { useState } from 'react'
+import { database } from "../../firebase"
 
 export default function Post({ post }) {
   const [comments, setComments] = useState([""])
+
+
+  function Like() {
+    //Find the post associated with this, then increment it's likes 
+    
+    database.posts.doc(post.id).update({
+      
+    })
+  }
+
+
   return (
     <>
     <Container>
 
+    <Button onClick={Like} variant="outline-success" size="lg">
+      Create a Post
+      </Button>
     
       <Card key={post.id} style={{}}>
         <Card.Body>
